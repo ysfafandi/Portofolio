3725756d239ceeb11f9238f475d1595bfa626c1b
ysfafandi <mukhamad.07@students.amikom.ac.id>
2025-11-13 16:22:02 +0700

    prosos penambahan dark mode

--- dist/script.js
+++ dist/script.js
@@ -2,19 +2,42 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTOp = document.querySelector("#to-top");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTOp.classList.remove("hidden");
+    toTOp.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    toTOp.classList.remove("flex");
+    toTOp.classList.add("hidden");
   }
 };
 
 // Hamburger
 const hamburger = document.querySelector("#hamburger");
-
 const navMenu = document.querySelector("#nav-menu");
+
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
+});
+
+// Klik di luar humburger nya
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// Dark Mode Toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+  } else html.classList.remove("dark");
 });
